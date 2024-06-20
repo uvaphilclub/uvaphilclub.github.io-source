98d95843a47556f6219e69c2acdfb4dc2f312de8
Grant Zou <grantbtzou@gmail.com>
2024-06-20 00:39:26 -0400

    added contemplative cavalier links to home

--- src/pages/index.js
+++ src/pages/index.js
@@ -178,18 +178,22 @@ const IndexPage = () =>
                 <div className="flex flex-wrap gap-x-20 mt-16 justify-center">
                 <FadeInLeftOnScroll delay={1000}>
                     <div className="w-64 self-center">
-                        <h1 className="hidden lg:block text-6xl italic">Special thanks to</h1>
+                        <h1 className="hidden lg:block text-6xl italic">Also visit</h1>
                     </div>
                 </FadeInLeftOnScroll>
                 <FadeInRightOnScroll delay={1000}>
-                    <div className="max-w-2xl font-medium bg-blue rounded p-6 sm:shadow-[20px_20px_0px_0_rgba(0,0,0,0.3)] font-['Shanti']">
-                        <h2 className="lg:hidden text-3xl text-white">Special thanks to</h2>
-                        <p className="text-lg text-white">
+                    <div className="max-w-2xl font-medium text-white bg-blue rounded p-6 sm:shadow-[20px_20px_0px_0_rgba(0,0,0,0.3)] font-['Shanti']">
+                        <h2 className="lg:hidden text-3xl">Also visit</h2>
+                        <h2 className="text-2xl lg:text-3xl">The Corcoran Department of Philosophy</h2>
+                        <p className="text-lg">
                             Special thanks to our official advisor Mark Jeter and the Corcoran Department of Philosophy for making this club possible!
                         </p>
-                        <p className="text-lg text-white">
+                        <p className="text-lg">
                             Learn more about the UVA Department of Philosophy at: <span><a className="underline font-bold text-xl" href="https://philosophy.virginia.edu/">https://philosophy.virginia.edu/</a></span>
                         </p> 
+                        <h2 className="text-2xl lg:text-3xl mt-4">The Contemplative Cavalier</h2>
+                        <p className="text-lg">UVA's premier undergraudate philosophy journal. All UVA students are welcome to submit.</p>
+                        <p className="text-lg"> To read or submit visit <a className="underline font-bold text-xl" href="https://the-contemplative-cavalier.vercel.app">https://the-contemplative-cavalier.vercel.app</a></p>
                     </div>
                 </FadeInRightOnScroll>
                 </div>
